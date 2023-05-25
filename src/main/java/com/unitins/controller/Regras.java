f3b57c076a50d62499f7e6e5f670e06d91c30722
Iago Leobas <iago.leobass@gmail.com>
2023-05-25 09:23:44 -0300

    updates

---
+++ src/main/java/com/unitins/controller/Regras.java
@@ -0,0 +1,36 @@
+package com.unitins.controller;
+
+public class Regras {
+
+	public static void main(String[] args) {
+
+		 String numero = "14159265358979323846";
+	        verificarCaracteresPrimos(numero);
+	    }
+	    
+	    public static void verificarCaracteresPrimos(String numero) {
+	        for (int i = 0; i < numero.length(); i++) {
+	            char caractere = numero.charAt(i);
+	            
+	            if (verificarPrimo(caractere)) {
+	                System.out.print(caractere + " ");
+	            }
+	        }
+	    }
+	    
+	    public static boolean verificarPrimo(char caractere) {
+	        int valor = Character.getNumericValue(caractere);
+	        
+	        if (valor <= 1) {
+	            return false;
+	        }
+	        
+	        for (int i = 2; i <= Math.sqrt(valor); i++) {
+	            if (valor % i == 0) {
+	                return false;
+	            }
+	        }
+	        
+	        return true;
+	    }
+	}
